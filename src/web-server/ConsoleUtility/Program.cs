8739e742541f1b69a75fce223bd7926b561f3f39
James Faix <jamesthefaix@gmail.com>
2019-08-20 22:21:04 -0400

    Added more C# dependencies

---
+++ src/web-server/ConsoleUtility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
